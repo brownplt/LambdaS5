1a2229ae1b9e60ccd9f23609636e03621fca5a7a
Arjun Guha <arjun@cs.brown.edu>
2011-04-12 16:04:48 -0400

    hello world program that links to JSON parser

---
+++ src/json_type.ml
@@ -0,0 +1,14 @@
+type json_type =
+    Object of (string * json_type) list
+  | Array of json_type list
+  | String of string
+  | Int of int
+  | Float of float
+  | Bool of bool
+  | Null
+
+type t = json_type
+
+exception Json_error of string
+
+let compare = Pervasives.compare
