d932fab3b807a953614f2353037b3fd03836b07c
Matt Carroll <mjc1283@gmail.com>
2011-04-14 23:21:18 -0400

    more desugars

--- src/js_to_exprjs.ml
+++ src/js_to_exprjs.ml
@@ -2,12 +2,38 @@ module E = Exprjs_syntax
 module J = Js_syntax
 
 let rec jse_to_exprjs (e : J.expr) : E.expr =
-        match e with
-        | J.This (p) -> E.ThisExpr (p)
-        | _ -> failwith "unimplemented expression type"
+  match e with
+    | J.This (p) -> E.ThisExpr (p)
+    | J.Id (p, i) -> E.IdExpr(p, i)
+    | J.Lit (p, l) -> 
+      let result = match l with 
+        | J.Null -> E.Null (p)
+        | J.Bool (b) -> if b then E.True (p) else E.False (p)
+        | J.Num (n) -> E.Num (p, n)
+        | J.Str (s) -> E.String (p, s) in result
+    | J.Array (p, el) -> E.ArrayExpr (p, List.map jse_to_exprjs el)
+    | J.Object (p, mem_lst) ->
+      let rec prop_to_str prop = match prop with
+        (J.PropId s | J.PropStr s) -> s
+        | J.PropNum n -> string_of_float n
+      and m_to_pr m = match m with
+        | J.Field (name, e) -> 
+          (p, prop_to_str name, E.Data (jse_to_exprjs e))
+        | J.Get (name, sel) -> failwith "not done yet"
+        | J.Set (name, arg, sel) -> failwith "not done yet" in
+      E.ObjectExpr(p, List.map m_to_pr mem_lst)
+    | J.Bracket (p, e1, e2) -> 
+      E.BracketExpr (p, jse_to_exprjs e1, jse_to_exprjs e2)
+    | _ -> failwith "unimplemented expression type"
 
 let rec jss_to_exprjs (s : J.stmt) : E.expr = 
         match s with
+        | J.Block (p, bl) -> 
+            let rec unroll sl = match sl with
+              | [] -> E.Undefined (p)
+              | [f] -> jss_to_exprjs f
+              | f :: rest -> E.SeqExpr (p, jss_to_exprjs f, unroll rest) in
+            unroll bl
         | J.For (p, e1, e2, e3, body) -> 
           let rec init1 a = match a with 
             None -> E.Undefined p 
