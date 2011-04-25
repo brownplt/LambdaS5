8a9f432a6867411062a8f18cb4945ade637ea4e1
Matt Carroll <mjc1283@gmail.com>
2011-04-25 11:52:57 -0400

    get rid of context arg in js_to_exprjs

--- src/js_to_exprjs.ml
+++ src/js_to_exprjs.ml
@@ -25,7 +25,7 @@ open Js_print
   | Call of pos * expr * expr list
   *)
 
-let rec jse_to_exprjs (e : J.expr) (context : E.expr) : E.expr =
+let rec jse_to_exprjs (e : J.expr) : E.expr =
   match e with
     | J.This (p) -> E.ThisExpr (p)
     | J.Id (p, i) -> (*E.IdExpr(p, i)*)
@@ -37,30 +37,28 @@ let rec jse_to_exprjs (e : J.expr) (context : E.expr) : E.expr =
         | J.Num (n) -> E.Num (p, n)
         | J.Str (s) -> E.String (p, s) in result
     | J.Array (p, el) -> 
-      E.ArrayExpr (p, List.map (fun x -> jse_to_exprjs x context) el)
+      E.ArrayExpr (p, List.map (fun x -> jse_to_exprjs x) el)
     | J.Object (p, mem_lst) ->
       let rec prop_to_str prop = match prop with
         (J.PropId s | J.PropStr s) -> s
         | J.PropNum n -> string_of_float n
       and m_to_pr m = match m with
         | J.Field (name, e) -> 
-          (p, prop_to_str name, E.Data (jse_to_exprjs e context))
-        | J.Get (name, body) -> let ns = prop_to_str name in
-          (p, ns, E.Getter (ns, E.FuncExpr (p, [], srcElts body context)))
-        | J.Set (name, arg, body) -> let ns = prop_to_str name in
-          (p, ns, E.Setter (ns, E.FuncExpr (p, [arg], srcElts body context))) in
+          (p, prop_to_str name, E.Data (jse_to_exprjs e))
+        | _ -> failwith "getter/setter nyi" in
       E.ObjectExpr(p, List.map m_to_pr mem_lst)
       (*TODO: below handling of J.Func is wrong, fix*)
-    | J.Func (p, nm, args, body) -> E.FuncExpr (p, args, srcElts body context)
+    | J.Func (p, nm, args, body) -> let parent = E.IdExpr (p, "%context") in
+      E.FuncExpr (p, args, srcElts body parent)
     | J.Bracket (p, e1, e2) -> 
-      E.BracketExpr (p, jse_to_exprjs e1 context, jse_to_exprjs e2 context)
+      E.BracketExpr (p, jse_to_exprjs e1, jse_to_exprjs e2)
     | J.Dot (p, e, nm) ->
-      E.BracketExpr (p, jse_to_exprjs e context, E.String (p, nm))
+      E.BracketExpr (p, jse_to_exprjs e, E.String (p, nm))
     | J.Infix (p, iop, l, r) ->
-      E.InfixExpr (p, iop, jse_to_exprjs l context, jse_to_exprjs r context)
+      E.InfixExpr (p, iop, jse_to_exprjs l, jse_to_exprjs r)
     | J.Assign (p, aop, l, r) -> 
-      let el = jse_to_exprjs l context
-      and er = jse_to_exprjs r context in
+      let el = jse_to_exprjs l
+      and er = jse_to_exprjs r in
       let target = match el with
         | E.BracketExpr (p, ll, rr) -> ll
         | _ -> E.IdExpr (p, "%context")
@@ -71,31 +69,31 @@ let rec jse_to_exprjs (e : J.expr) (context : E.expr) : E.expr =
         | "=" -> E.AssignExpr (p, target, property, er)
         | _ -> failwith "only assign op implemented is =" in
       e_asgn
-    | J.Call (p, e, el) -> let xl = List.map (fun x -> jse_to_exprjs x context) el in 
-      E.AppExpr (p, jse_to_exprjs e context, xl)
+    | J.Call (p, e, el) -> let xl = List.map (fun x -> jse_to_exprjs x) el in 
+      E.AppExpr (p, jse_to_exprjs e, xl)
     | e -> failwith "unimplemented expression type"
 
-and jss_to_exprjs (s : J.stmt) (context : E.expr) : E.expr = 
+and jss_to_exprjs (s : J.stmt) : E.expr = 
   match s with
   | J.Block (p, sl) ->
     let rec unroll = function
       | [] -> E.Undefined (p)
-      | [f] -> jss_to_exprjs f context
-      | f :: rest -> E.SeqExpr (p, jss_to_exprjs f context, unroll rest) in
+      | [f] -> jss_to_exprjs f
+      | f :: rest -> E.SeqExpr (p, jss_to_exprjs f, unroll rest) in
     unroll sl
   | J.Var (p, vdl) -> 
     let rec vdj_to_vde v = match v with
       | J.VarDecl (id, e) -> match e with
         | None -> E.AssignExpr (p, E.IdExpr (p, "%context"), E.String (p, id), E.Undefined (p)) 
-        | Some x -> let r = jse_to_exprjs x context in 
+        | Some x -> let r = jse_to_exprjs x in 
           E.AssignExpr (p, E.IdExpr (p, "%context"), E.String (p, id), r)
     and unroll vl = match vl with
       | [] -> E.Undefined (p)
       | [f] -> vdj_to_vde f
       | f :: rest -> E.SeqExpr(p, vdj_to_vde f, unroll rest) in
     unroll vdl
-  | J.Expr (p, e) -> jse_to_exprjs e context
-  | J.Labeled (p, id, s) -> E.LabelledExpr (p, id, jss_to_exprjs s context)
+  | J.Expr (p, e) -> jse_to_exprjs e
+  | J.Labeled (p, id, s) -> E.LabelledExpr (p, id, jss_to_exprjs s)
   | J.Continue (p, id) -> let lbl = match id with
     | None -> "loopstart" | Some s -> s in
     E.BreakExpr (p, lbl, E.Undefined (p))
@@ -104,18 +102,18 @@ and jss_to_exprjs (s : J.stmt) (context : E.expr) : E.expr =
     E.BreakExpr (p, lbl, E.Undefined (p))
   | J.Return (p, e) -> let rval = match e with
     | None -> E.Undefined (p)
-    | Some x -> jse_to_exprjs x context in
+    | Some x -> jse_to_exprjs x in
     E.BreakExpr (p, "%ret", rval)
   | J.For (p, e1, e2, e3, body) -> 
     let rec init1 a = match a with 
       | None -> E.Undefined (p)
-      | Some a -> jse_to_exprjs a context
+      | Some a -> jse_to_exprjs a
     and init2 b = match b with
       | None -> E.True (p)
-      | Some b -> jse_to_exprjs b context in
+      | Some b -> jse_to_exprjs b in
     let innerwhile = 
       E.WhileExpr (p, init2 e2, 
-        E.SeqExpr (p, E.LabelledExpr (p, "loopstart", jss_to_exprjs body context), 
+        E.SeqExpr (p, E.LabelledExpr (p, "loopstart", jss_to_exprjs body), 
         init1 e3)) in
     E.SeqExpr (p, init1 e1,
       E.SeqExpr (p, innerwhile,
@@ -128,7 +126,7 @@ and srcElts (ss : J.srcElt list) (parent : E.expr) : E.expr =
   and context = E.ObjectExpr (p, [parent_prop])
   and srcElts (ss : J.srcElt list) (parent : E.expr) : E.expr =
     let se_to_e se = match se with
-      | J.Stmt (s) -> jss_to_exprjs s context
+      | J.Stmt (s) -> jss_to_exprjs s
       | J.FuncDecl (nm, args, body) ->
         E.FuncStmtExpr (p, nm, args, srcElts body context) in
      match ss with
@@ -136,4 +134,3 @@ and srcElts (ss : J.srcElt list) (parent : E.expr) : E.expr =
       | [first] -> se_to_e first
       | first :: rest -> E.SeqExpr (p, se_to_e first, srcElts rest parent) in
   E.LetExpr (p, "%context", context, srcElts ss parent)
-
