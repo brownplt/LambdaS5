2beef3aa50a4f76c60c62176ddba9b02a72dc704
joseph <joe@cs.brown.edu>
2011-05-01 13:02:12 -0400

    cleaning up tests directory

--- tests/get-test262.sh
+++ tests/get-test262.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 wget -O test262.tgz http://hg.ecmascript.org/tests/test262/archive/tip.tar.gz
-tar xzf test262.tgz
+tar xzf test262.tgz --directory=./test262
+cp -r ./test262/test262-*/* ./test262/test262/
+rm -rf ./test262/test262-*
 
