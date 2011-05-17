6541953008b781fadd3ac2f66ef3091fecdaff7c
Joe Politz <joe@cs.brown.edu>
2011-05-17 15:29:09 -0400

    deleting temp files means no need for /ltmp

--- tests/test262/single-test.sh
+++ tests/test262/single-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-JSFILE=`mktemp -p /ltmp`
-NOCOMMENTS=`mktemp -p /ltmp`
+JSFILE=`mktemp`
+NOCOMMENTS=`mktemp`
 cat test262/test/harness/sta.js >> $JSFILE
 cat S5_harness_before.js >> $JSFILE
 cat $1 >> $JSFILE
@@ -14,35 +14,41 @@ cat $JSFILE > out1.txt
 cat $NOCOMMENTS > out.txt
 
 
-JSONFILE=`mktemp -p /ltmp`
+JSONFILE=`mktemp`
 ../../bin/js -e "print(JSON.stringify(Reflect.parse(read('$NOCOMMENTS'),{loc:true}),function(key,value){if(key==='value'&&(value)instanceof(RegExp)){return{re_lit:String(value)}}return(value)},2))" > $JSONFILE
 
 cat $JSONFILE > out2.txt
 
-RESULT=`mktemp -p /ltmp`
+RESULT=`mktemp`
 
 ocamlrun ../../src/s5.d.byte -desugar $JSONFILE -env ../../envs/es5.env -eval &> $RESULT
 
 cat $RESULT
 
 if grep -q "HARNESS: Passed" $RESULT; then
+    rm -f $RESULT
     exit 0
 fi
 
 if grep -q "HARNESS: Failed" $RESULT; then
+    rm -f $RESULT
     exit 4
 fi
 
 if grep -q "%%No eval yet%%" $RESULT; then
+    rm -f $RESULT
     exit 3
 fi
 
 if grep -q "Json_type.Json_error" $RESULT; then
+    rm -f $RESULT
     exit 2
 fi
 
 if grep -1 "Failure" $RESULT; then
+    rm -f $RESULT
     exit 1
 fi
 
+rm -f $RESULT
 exit 5
