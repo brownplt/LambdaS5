e30ce0318acfc7f486c764903b729ab9909beb13
Matt Carroll <mjc1283@gmail.com>
2011-09-05 10:31:23 -0400

    new x throws TypeError if x isn't a function

--- src/exprjs_to_ljs.ml
+++ src/exprjs_to_ljs.ml
@@ -167,6 +167,12 @@ let rec exprjs_to_ljs (e : E.expr) : S.exp = match e with
     let getterargs = S.Object (p, S.d_attrs, []) in
     let constrargs = make_args_obj p (map exprjs_to_ljs eargs) in
     S.Let (p, constr_id, exprjs_to_ljs econstr,
+      S.If (p, 
+        S.Op1 (p, "!", 
+          S.Op2 (p, "stx=", 
+            S.Op1 (p, "typeof", S.Id (p, constr_id)),
+            S.String (p, "function"))),
+        S.App (p, S.Id (p, "%ThrowTypeError"), [S.Null (p); S.Null (p)]),
            S.Let (p, pr_id, S.GetField (p, S.Id (p, constr_id), 
                                         S.String (p, "prototype"), getterargs),
                   S.If (p, S.Op2 (p, "stx=", S.Id (p, pr_id), S.Undefined (p)), 
@@ -184,7 +190,7 @@ let rec exprjs_to_ljs (e : E.expr) : S.exp = match e with
                                       S.If (p, S.Op2 (p, "stx=", S.String (p,
                                       "function"), S.Op1 (p, "typeof", S.Id (p,
                                       constr_result))), S.Id (p, constr_result),
-                                      S.Id (p, newobj)))))))))
+                                      S.Id (p, newobj))))))))))
   | E.PrefixExpr (p, op, exp) -> let result = match op with
     | "postfix:++" ->
       begin match exp with
