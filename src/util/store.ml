0e4333ebe8f3541b2cf84a617e0921b3e085809e
Ben Lerner <blerner@cs.washington.edu>
2012-01-18 11:08:16 -0500

    adding a functional Store implementation: abstract locations, polymorphic values, alloc and free

---
+++ src/util/store.ml
@@ -0,0 +1,35 @@
+open Format
+open FormatExt
+open Prelude
+
+let loc = ref 0
+
+module Loc = struct
+  type t = int
+  let compare = Pervasives.compare
+end
+module LocMap = Map.Make (Loc)
+type loc = int
+let newLoc () : Loc.t =
+  loc := !loc + 1;
+  !loc
+let distinct loc1 loc2 = (loc1 <> loc2)
+let print_loc loc = "L" ^ (string_of_int loc)
+type 'a t = 'a LocMap.t
+let empty = LocMap.empty
+let alloc valu store = 
+  let loc = newLoc () in 
+  (loc, LocMap.add loc valu store)
+let free = LocMap.remove 
+let mem = LocMap.mem
+let lookup = LocMap.find 
+let iter = LocMap.iter
+let fold = LocMap.fold
+let for_all = LocMap.for_all
+let exists = LocMap.exists
+let filter = LocMap.filter
+let partition = LocMap.partition
+let cardinal = LocMap.cardinal
+let bindings = LocMap.bindings
+let map = LocMap.map
+let mapi = LocMap.mapi
---
+++ src/util/store.mli
@@ -0,0 +1,23 @@
+open Format
+open FormatExt
+open Prelude
+
+type loc
+type +'a t
+val distinct : loc -> loc -> bool
+val print_loc : loc -> string
+val empty : 'a t
+val alloc : 'a -> 'a t -> loc * 'a t
+val free : loc -> 'a t -> 'a t
+val mem : loc -> 'a t -> bool
+val lookup : loc -> 'a t -> 'a
+val iter : (loc -> 'a -> unit) -> 'a t -> unit
+val fold : (loc -> 'a -> 'b -> 'b) -> 'a t -> 'b -> 'b
+val for_all : (loc -> 'a -> bool) -> 'a t -> bool
+val exists : (loc -> 'a -> bool) -> 'a t -> bool
+val filter : (loc -> 'a -> bool) -> 'a t -> 'a t
+val partition : (loc -> 'a -> bool) -> 'a t -> 'a t * 'a t
+val cardinal : 'a t -> int
+val bindings : 'a t -> (loc * 'a) list
+val map : ('a -> 'b) -> 'a t -> 'b t
+val mapi : (loc -> 'a -> 'b) -> 'a t -> 'b t
