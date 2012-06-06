d32005eb9636a7ca1cd3ba0022f805c3db20f635
joseph <joe@cs.brown.edu>
2012-06-06 09:00:01 -0400

    desugar in the right directory w/relative paths

---
+++ tests/desugar.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo $1 > file2.js
+cat $1 > file.js
+
+../bin/js -e "print(JSON.stringify(Reflect.parse(read('$1'),{loc:true}),function(key,value){if(key==='value'&&(value)instanceof(RegExp)){return{re_lit:String(value)}}return(value)},2))"
