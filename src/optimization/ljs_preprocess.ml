8891f7f9c34597851e6f99ea294b217edfb36800
Junsong Li <ljs.darkfish@gmail.com>
2014-10-07 09:34:57 -0400

    fix bugs that failed to recognize set-property on context and resulted in infinte loop

--- src/optimization/ljs_preprocess.ml
+++ src/optimization/ljs_preprocess.ml
@@ -133,34 +133,122 @@ let replace_let_body let_exp new_body =
   in 
   traverse let_exp
   
+(* these function will check if the program contains assignment to "this" or "window".
+   Such assignments will create new variables silently. To be more specific, this 
+   function will look for
+   1. %context["window"]: code may do bad things through window
+   2. %EnvCheckAssign(_, _, %this, _): code may do bad things through the alias of 'this' 
+   3. passing %this to a function: code may do something like `function z(a) {a.x = 1}; z(this)`
+   4. string computation in field
+
+   5. x++, x--, ++x, --x: this will be desugared to %PostIncrement(%context, "x"), we don't have corresponding operation on S5 identifiers.
+
+todo: in strict mode
+*)
+let rec eligible_for_preprocess exp : bool = 
+  (*let apply_to_attr (attr : attrs) = 
+    let apply_to_option (opt : exp option) : bool = match opt with
+      | Some(e) -> eligible_for_preprocess e
+      | None -> true
+    in 
+    apply_to_option attr.primval &&
+    apply_to_option attr.code &&
+    apply_to_option attr.proto 
+  in 
+  let rec apply_to_props (props : (string * prop) list) : bool =
+    let handle_prop p = match p with
+      | (_, Data (data, _, _)) -> 
+        eligible_for_preprocess data.value
+      | (_, Accessor (acc, _, _)) ->
+        eligible_for_preprocess acc.getter &&
+        eligible_for_preprocess acc.setter
+    in
+    List.for_all handle_prop props
+  in *)
+  let is_static_field fld = match fld with
+    | String (_, _) -> true
+    | _ -> false
+  in 
+  let rec pass_this_arg (args : exp list) = 
+    (* pass "%this" as the function arguments *)
+    let rec contain_this exp = match exp with
+      | Id (_, id) -> id = "%this"
+      | _ -> List.exists contain_this (child_exps exp)
+    in 
+    begin match args with
+    | [] -> false
+    | hd :: rest -> contain_this hd || pass_this_arg rest
+    end 
+  in 
+  match exp with
+  | Undefined _
+  | Null _
+  | String (_,_)
+  | Num (_,_)
+  | True _
+  | False _ 
+  | Id _ -> true
+  | GetField (_, obj, fld, args) ->
+    (match obj, fld with
+    | Id(_, "%context"), String(_, "window") -> false
+    | Id(_, "%context"), fld -> is_static_field fld
+    | _ -> 
+      eligible_for_preprocess obj &&
+      eligible_for_preprocess fld &&
+      eligible_for_preprocess args
+    )
+  | App (_, f, args) -> (match f, args with
+      | Id (_, "%EnvCheckAssign"), [_;_; Id(_, "%this");_] -> false
+        (* todo: we can translate this into s5! *)
+      | Id (_, "%PostIncrement"), _ -> false
+      | Id (_, "%PostDecrement"), _ -> false
+      | Id (_, "%PrefixDecrement"), _ -> false
+      | Id (_, "%PrefixIncrement"), _ -> false
+      | _ -> 
+        eligible_for_preprocess f && 
+        List.for_all eligible_for_preprocess args &&
+        not (pass_this_arg args)
+    )
+  | _ -> List.for_all eligible_for_preprocess (child_exps exp)
+  
 (* this phase highly relies on the desugared patterns.
    this phase must be the first phase before all optimizations.
 
    recognize the following pattern:
-   - %defineGlobalVar(%context, "x")
+   - %defineGlobalVar(%context, "x") 
+     => let (x = undefined) ...
    - %context["x"] if x in %context
+     => x or x's actual binding location
    - %context["x" = ..] if "x" in %context
+     => x := ... or mutation on x's actual binding identifiers 
    - in function object: let {%context = {let..let..let { contextobj}} function-body}
          remove the %context
-   - this.x (will be desugared to %PropAccessorCheck(%this), w
+   - %PropAccessorCheck(%this) in top-level
+     => %context
+     therefore, this.x, which will be desugared to %PropAccessorCheck(%this)["x"..], will be
+     translated to %context["x"]
+
+
+todo: think about the pattern
+    %set-property(%context, "x", 1), maybe to %context["x"=1] and then x := 1?
 *)
 let preprocess (e : exp) : exp =
-  let rec preprocess_rec (e : exp) (ctx : env) : exp = 
+  let rec preprocess_rec ?(in_lambda=false) (e : exp) (ctx : env) : exp = 
     match e with 
     | Seq (p, e1, e2) ->
       (match e1 with
        | App (_, Id (_, "%defineGlobalVar"), [Id(_,_); String (_, id)]) ->
          let ctx = IdMap.add id (Undefined Pos.dummy) ctx in
-         let newe2 = preprocess_rec e2 ctx in
+         let newe2 = preprocess_rec ~in_lambda e2 ctx in
          Let (p, id, Undefined Pos.dummy, newe2)
        | _ -> 
-         let newe1 = preprocess_rec e1 ctx in
-         let newe2 = preprocess_rec e2 ctx in
+         let newe1 = preprocess_rec ~in_lambda e1 ctx in
+         let newe2 = preprocess_rec ~in_lambda e2 ctx in
          Seq (p, newe1, newe2))
     | GetField (pos, obj, fld, args) ->
-      let o = preprocess_rec obj ctx in
-      let f = preprocess_rec fld ctx in
-      let a = preprocess_rec args ctx in
+      let o = preprocess_rec ~in_lambda obj ctx in
+      let f = preprocess_rec ~in_lambda fld ctx in
+      let a = preprocess_rec ~in_lambda args ctx in
       (match o, f with
       | Id (_, "%context"), String (_, fldstr) -> (* get fld from context *)
         (*printf "match context['%s']\n%!" fldstr;
@@ -169,16 +257,18 @@ let preprocess (e : exp) : exp =
           match IdMap.find fldstr ctx with
           | Undefined _ -> Id (Pos.dummy, fldstr)
           | Id (_,_) as id -> id
-          | _ -> failwith "GetField: how could IdMap stores unrecognized exp"
+          | e -> 
+            (* printf "not expecting: %s\n%!" (Exp_util.ljs_str e); *)
+            e
         with Not_found -> GetField (pos, o, f, a)
         )
       | _ -> GetField (pos, o, f, a)
       )
     | SetField (pos, obj, fld, newval, args) ->
-      let o = preprocess_rec obj ctx in
-      let f = preprocess_rec fld ctx in
-      let v = preprocess_rec newval ctx in
-      let a = preprocess_rec args ctx in
+      let o = preprocess_rec ~in_lambda obj ctx in
+      let f = preprocess_rec ~in_lambda fld ctx in
+      let v = preprocess_rec ~in_lambda newval ctx in
+      let a = preprocess_rec ~in_lambda args ctx in
       (match o, f with
        | Id (_, "%context"), String (_, fldstr) ->
          (try match IdMap.find fldstr ctx with
@@ -190,36 +280,47 @@ let preprocess (e : exp) : exp =
        | _ -> SetField (pos, o, f, v, a)
       )
     | App (pos, f, args) ->
-      let f = preprocess_rec f ctx in
-      let args = List.map (fun x->preprocess_rec x ctx) args in
+      let f = preprocess_rec ~in_lambda f ctx in
+      let args = List.map (fun x->preprocess_rec ~in_lambda x ctx) args in
       (match f, args with 
-       | Id (_, "%EnvCheckAssign"),
-         [Id (_, "%context"); String (_, id); v; _] ->
+       | Id (_, "%EnvCheckAssign"), [Id (_, "%context"); String (_, id); v; _] ->
          (try match IdMap.find id ctx with
             | Undefined _ -> SetBang (pos, id, v)
             | Id (_, actual_id) -> SetBang (pos, actual_id, v)
             | _ -> failwith "EnvCheckAssign: how could IdMap stores unrecognized exp"
-          with Not_found -> App (pos, f, args)
+          with Not_found -> App (pos, f, args))
+       | Id (_, "%PropAccessorCheck"), [Id (_, "%this")] ->
+         if in_lambda then
+           App (pos, f, args)
+         else 
+           Id (pos, "%context")
+       | Id (p1, "%set-property"), [Id (p2, "%context"); String (p3, id); v] ->
+         let newexp = SetField (p1, Id(p2, "%context"), String(p3,id), v, Null Pos.dummy) in
+         (match preprocess_rec ~in_lambda newexp ctx, newexp with
+          | SetField(_,_,_,_,_), SetField(_,_,_,_,_) ->
+            (* cannot translate, use the original set-property exp *)
+            App (pos, f, args)
+          | result, _ -> result
          )
        | _ -> App (pos, f, args)
       )
     | Let (p, x, x_v, body) ->
-      let x_v = preprocess_rec x_v ctx in
+      let x_v = preprocess_rec ~in_lambda x_v ctx in
       begin match get_localcontext e with
       | None -> (* not a new context binding *)
-        Let (p, x, x_v, preprocess_rec body ctx)
+        Let (p, x, x_v, preprocess_rec ~in_lambda body ctx)
       | Some (new_let)->
         (try
            let new_ctx = recognize_new_context x_v ctx in 
-           replace_let_body new_let (preprocess_rec body new_ctx)
+           replace_let_body new_let (preprocess_rec ~in_lambda body new_ctx)
          with Failure msg -> 
            (printf "fail %s\n%!" msg;
-            Let (p, x, x_v, preprocess_rec body ctx)
+            Let (p, x, x_v, preprocess_rec ~in_lambda body ctx)
            )
         )
       end 
     | Lambda (p, xs, body) ->
-      let result = preprocess_rec body ctx in 
+      let result = preprocess_rec ~in_lambda:true body ctx in 
       Lambda (p, xs, result)
     | Undefined _ 
     | Null _ 
@@ -246,7 +347,10 @@ let preprocess (e : exp) : exp =
     | TryFinally (_,_,_)
     | Throw (_,_)
     | Hint (_,_,_)
-      -> optimize (fun e->preprocess_rec e ctx) e
+      -> optimize (fun e->preprocess_rec ~in_lambda e ctx) e
   in 
-  let env = IdMap.empty in
-  preprocess_rec e env
+  if eligible_for_preprocess e then
+    let env = IdMap.empty in
+    preprocess_rec e env
+  else 
+    e
