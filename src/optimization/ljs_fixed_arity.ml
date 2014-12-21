f70cf929d8287a20eb63f72cff204da119df2ba3
lijunsong <ljs.darkfish@gmail.com>
2014-12-20 21:30:10 -0500

    clean arguments keywords in fixed-arity

--- src/optimization/ljs_fixed_arity.ml
+++ src/optimization/ljs_fixed_arity.ml
@@ -3,7 +3,7 @@ open Ljs_syntax
 open Ljs_opt
 
 (* take out argument declaration exp, and return ordered formal
-   parameters *)
+   parameter list *)
 let trim_args (exp : exp) : id list * exp =
   let get_args e =
     match e with
@@ -47,6 +47,9 @@ let update_args (old_xs : id list) (new_xs : id list) =
     hd :: new_xs
   | _ -> old_xs
 
+(* mkArgsObj is called on an object which has the arguments as properties.
+   This function will extract those arguments.
+ *)
 let parse_args_obj (args : exp list) : exp list option =
   let get_field_values (obj : exp) : exp list option =
     let rec get_values props = match props with
@@ -73,8 +76,23 @@ let parse_args_obj (args : exp list) : exp list option =
       | None -> None
     end
   | _ -> None
-  
+
+(* delete one field whose name is given by string fld *)
+let delete_field (fld : string) (obj : exp) : exp =
+  let rec delete (fld : string) (props : (string * prop) list) : (string * prop) list =
+    match props with
+    | [] -> []
+    | (str, p) :: tl when str = fld -> delete fld tl
+    | hd :: tl ->
+      hd :: (delete fld tl)
+  in
+  match obj with
+  | Object (p, a, props) ->
+    Object (p, a, delete fld props)
+  | _ -> obj
+    
 let fixed_arity (exp : exp) : exp =
+  (* clean formal parameter *)
   let rec fixed_formal_parameter (exp : exp) : exp =
     (* clean related %args expression *)
     let rec clean_args (fbody : exp) : exp =
@@ -83,19 +101,35 @@ let fixed_arity (exp : exp) : exp =
         | DeleteField(_, Id(_, "%args"), String (_, "%new")) -> true
         | _ -> false
       in
+      let rec clean_arguments (exp : exp) : exp =
+        (* clean "arguments" property in context *)
+        match exp with
+        | Let (p, "%context", xv, body) ->
+          Let (p, "%context", clean_arguments xv, body)
+        | Let (p, x, xv, body) ->
+          Let (p, x, xv, clean_arguments body)
+        | Object (_, _, _) -> delete_field "arguments" exp
+        | Lambda (_, _, _) -> exp
+        | _ -> optimize clean_arguments exp
+      in
       match fbody with
-      | Seq (_, e1, e2) when is_arg_delete e1 -> e2
+      | Seq (_, e1, e2) when is_arg_delete e1 ->
+        clean_arguments e2
       | _ -> fbody
     in
     match exp with
     | Lambda (pos, xs, body) ->
+      (* take out those Let bindings in function body *)
       let args, new_body = trim_args body in
+      (* make a new formal parameter list *)
       let new_xs = update_args xs args in
+      (* clean %args in the body, and "arguments" property in context(if exists)*)
       let new_body = clean_args new_body in
       Lambda (pos, new_xs, fixed_formal_parameter new_body)
     | _ ->
       optimize fixed_formal_parameter exp
   in
+  (* clean call site *)
   let rec fixed_call_site (exp : exp) : exp =
     match exp with
     | App (p, f, args) ->
