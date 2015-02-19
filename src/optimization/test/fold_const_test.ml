e238baa57e68d6ae9b461be8dea8523da919120b
lijunsong <ljs.darkfish@gmail.com>
2015-02-19 14:03:21 -0500

    change const folding

--- src/optimization/test/fold_const_test.ml
+++ src/optimization/test/fold_const_test.ml
@@ -6,51 +6,8 @@ open Ljs_fold_const
 let suite = 
   let cmp before after = cmp before fold_const after in
   let no_change code = no_change code fold_const in
-  let obj = "{[#proto: null, #extensible: false, #class: 'Object']
-             'fld1': {#value 1, #writable false},
-             'fld2': {#getter func(this, arg) {1}, #setter func(t) {t}}}" 
-  in
-  let sideeffect = "{[#proto: null, #extensible: false, #class: 'Object']
-                    'fld1': {#value prim('pretty', 1), #writable false},
-                    'fld2': {#getter func(this, arg) {1}, #setter func(t) {t}}}"
-  in 
   "Test Const Folding" >:::
     [
-      "fold getobjattr" >::
-        (cmp (obj ^ "[<#extensible>]")
-             "false");
-
-      "fold getobjattr on side-effect obj" >::
-        (no_change (sideeffect ^ "[<#extensible>]"));
-
-      (* ---------------------------- *)
-      "fold getattr" >::
-        (cmp (obj ^ "['fld1'<#value>]")
-             "1");
-
-      "fold getattr field not present" >::
-        (cmp (obj ^ "['fld3'<#value>]")
-             "undefined");
-
-      "fold getattr on side-effect object" >::
-        (no_change (sideeffect ^ "['fld1'<#value>]"));
-      
-      (* ---------------------------- *)
-      "get field" >::
-        (cmp (obj ^ "['fld1']")
-             "1");
-      
-      "get a field that has getter" >:: 
-        (cmp (obj ^ "['fld2']")
-             "func(this, arg){1}({[]})");
-
-      "get a field on side-effect obj" >::
-        (no_change (sideeffect ^ "['fld1']"));
-
-      "get a field that not exists" >::
-        (cmp (obj ^ "['fld3']")
-             "undefined");
-
       (* ---------------------------- *)
       "op1" >::
         (cmp "prim('typeof', 1)"
@@ -65,7 +22,6 @@ let suite =
       "op1 has sideeffect" >::
         (no_change "prim('pretty', 1)");
 
-      (* ---------------------------- *)
       "if" >::
         (cmp "if (prim('+',1,2)) {1} else {2}" "2");
       "if" >::
@@ -87,34 +43,6 @@ let suite =
         (no_change "let (r = 1)
                     rec (r = func(t) { r(prim('-',t,1))})
                     r(x)");
-
-      (* ------------------------------ *)
-      "look through proto" >::
-        (cmp "{[#extensible: false, 
-                #proto: {[#extensible: false,
-                          #proto: {[#extensible: false,
-                                    #proto: {[#extensible: false,
-                                              #proto: {[#extensible: false]}]}]
-                                    'fld1': {#value 1, #writable false}}]
-                          'fld2': {#value 2, #writable false}}]
-               'fld3': {#value 3, #writable false}}['fld1']"
-             "1");
-                      
-      "label and break" >::
-      (cmp "label ret : {
-            break ret {[]} }"
-           "{[]}");
-
-
-      "label and break" >::
-      (no_change "label ret : {
-                  if (t === 3) {
-                     break ret {[]} 
-                  } else {
-                     break ret 1
-                  }}");
-
-
     ]
 
 let _ =
