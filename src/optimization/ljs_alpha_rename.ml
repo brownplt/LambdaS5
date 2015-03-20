955137547fca917d168f0eb882a6b7b88b210314
lijunsong <ljs.darkfish@gmail.com>
2015-03-20 15:41:32 -0400

    alpha rename done

--- src/optimization/ljs_alpha_rename.ml
+++ src/optimization/ljs_alpha_rename.ml
@@ -1,3 +1,4 @@
+open Prelude
 open Ljs_syntax
 open Ljs_opt
 
@@ -12,40 +13,97 @@ let is_empty_inter s1 s2 =
 (* env is used for name replacement: old name => new name *)
 type env = id IdMap.t
 
+let env_lookup id env : id = 
+  IdMap.find id env
 let env_mem id env = IdMap.mem id env
 let env_drop id env = IdMap.filter (fun x _ -> not (x = id)) env
-    
-let alpha_rename (func : exp) (namespace : IdSet.t) : exp =
+let env_extend k v env = IdMap.add k v env
+let rec build_env oldnames newnames env =
+  match oldnames, newnames with
+  | [], [] -> env
+  | hd1 :: tl1, hd2 :: tl2 ->
+    build_env tl1 tl2 (env_extend hd1 hd2 env)
+  | _ -> failwith "build env failed: arity mismatch"
   
-  let rec resolve (exp : exp) (env : env) : exp =
-    let resolve_helper e = resolve e env in
+(* renaming strategy: ?*numeric => ?*(numeric+1) *)
+(* identifier always has non-numeric + numeric as its name *)
+let split_number (name : id) : (string * int) option =
+  if Str.string_match (Str.regexp "\\(.*[^0-9]\\)\\([0-9]+\\)$") name 0 then
+    let alphabet = Str.matched_group 1 name in
+    let num = Str.matched_group 2 name in
+    Some (alphabet, int_of_string num)
+  else
+    None
+
+(* return a fresh name as well a new space *)
+let rec fresh_name (name : id) (space : IdSet.t) : id * IdSet.t =
+  if not (IdSet.mem name space) then
+    name, IdSet.add name space
+  else
+    match split_number name with
+    | None -> fresh_name (name ^ "0") space
+    | Some (alphabet, num) ->
+      fresh_name (alphabet ^ (string_of_int (num + 1))) space
+
+let rec make_new_name (xs : id list) (namespace : IdSet.t) : id list * IdSet.t =
+  (* give a list of id, for each create a new one if necessary *)
+  match xs with
+  | [] -> [], namespace
+  | hd :: tl ->
+    if IdSet.mem hd namespace then (* name exists *)
+      let new_name, new_namespace = fresh_name hd namespace in
+      let _ = assert (not (IdSet.mem new_name namespace)) in
+      let new_tl, new_namespace = make_new_name tl new_namespace in
+      new_name :: new_tl, new_namespace
+    else
+      let new_namespace = IdSet.add hd namespace in
+      let new_tl, new_namespace = make_new_name tl new_namespace in
+      hd :: new_tl, new_namespace
+
+(* give an old name, return an fresh name that has name as prefix *)
+let alpha_rename (func : exp) (namespace : IdSet.t) : exp =
+  let rec resolve (exp : exp) (env : env) (namespace : IdSet.t) : exp =
+    let resolve_helper e = resolve e env namespace in
     match exp with
     | Id (p, id) when env_mem id env ->
-      Id (p, IdMap.find id name_mapping)
-    | Let (p, x, xv, body) when env_mem x env ->
+      Id (p, IdMap.find id env)
+    | SetBang (p, x, v) when env_mem x env ->
+      SetBang (p, env_lookup x env, resolve_helper v)
+    (*| Let (p, x, xv, body) when env_mem x env ->
       (* old name is rebound, so just leave it along *)
-      let new_xv = resolve xv env in
+      let new_xv = resolve xv env namespace in
       let new_env = env_drop x env in
-      let new_body = resolve body new_env in
-      Let (p, x, new_xv, new_body)
-    | Rec (p, x, xv, body) when env_mem x env ->
+      let new_body = resolve body new_env namespace in
+      Let (p, x, new_xv, new_body)*)
+    | Let (p, x, xv, body) when IdSet.mem x namespace ->
+      let new_xv = resolve xv env namespace in
+      let new_x, new_namespace = fresh_name x namespace in
+      let new_env = env_extend x new_x env in
+      let new_body = resolve body new_env new_namespace in
+      Let (p, new_x, new_xv, new_body)
+    (*| Rec (p, x, xv, body) when env_mem x env ->
       (* old name is rebound, so just leave it along *)
       (* the x in xv is bound to a new one, not the one we want to replace *)
       let new_env = env_drop x env in
-      let new_xv = resolve xv new_env in
-      let new_body = resolve body new_env in
-      Rec (p, x, new_xv, new_body)
-    | Lambda (p, xs, body) 
+      let new_xv = resolve xv new_env namespace in
+      let new_body = resolve body new_env namespace in
+      Rec (p, x, new_xv, new_body) *)
+    | Rec (p, x, xv, body) when IdSet.mem x namespace ->
+      let new_x, new_namespace = fresh_name x namespace in
+      let new_env = env_extend x new_x env in
+      let new_xv = resolve xv new_env new_namespace in
+      let new_body = resolve body new_env new_namespace in
+      Rec (p, new_x, new_xv, new_body)
+    | Lambda (p, xs, body) ->
+      (* rename xs in body to avoid conflict with namespace *)
+      let new_ids, new_namespace = make_new_name xs namespace in
+      let new_env = build_env xs new_ids env in
+      Lambda (p, new_ids, resolve body new_env new_namespace)
+    | _ -> optimize resolve_helper exp
   in
   (* first, let's make sure the function formal argument has conflict *)
   match func with
   | Lambda (p, xs, body) ->
-    let conflict_names = IdSet.inter (IdSet.from_list xs) conflict in
-    if IdSet.is_empty conflict_names then
-      (* no conflict, return the original form *)
-      func
-    else
-      let new_xs, new_body = resolve xs body namespace in
-      let fix_xs = 
-      Lambda (p, new_xs, new_body)
+    (* rename xs in body to avoid conflict with namespace *)
+    resolve func IdMap.empty namespace
   | _ -> failwith "alpha_rename only accepts function definition as the argument"
--- src/optimization/test/ljs_alpha_rename_test.ml
+++ src/optimization/test/ljs_alpha_rename_test.ml
@@ -3,49 +3,113 @@ open Util
 open OUnit2
 open Ljs_alpha_rename
 
-let suite =
-  let rename (space : id list) before after =
-    (* space: a list of id as the whole name space,
-       before: code before renaming
-       after: the expect code
-    *)
-    let idset = IdSet.from_list space in
-    cmp before (fun exp -> alpha_rename exp idset) after in
-  let no_change (space : id list) code =
-    no_change code (rename space) in
-  "Test Alpha renaming" >:::
 
-  [(rename
-      ["x"; "y"; "t"]
-      "func(x) { x := t }"
-      "func(x1) { x1 := t }");
-   
-   (rename
-      ["x"; "y"; "t"]
-      "func(x) {
+let suite =
+  let cmp (space : id list) before after =
+    let space = IdSet.from_list space in
+    cmp before (fun e -> alpha_rename e space) after in
+  let no_change (space : id list) code = 
+    let space = IdSet.from_list space in
+    no_change code (fun e -> alpha_rename e space) in 
+  let test_fresh (old_name : id) (space : id list) (expect_name : id) =
+    fun _ ->
+      let new_name, _ = fresh_name old_name (IdSet.from_list space) in
+      assert_equal expect_name new_name
+        ~printer: (fun s ->(sprintf "%s" s))
+  in
+  let test_rename_list (old_names : id list) (space : id list) (expect_names : id list) =
+    fun _ ->
+      let new_names, _ = make_new_name old_names (IdSet.from_list space) in
+      assert_equal expect_names new_names
+  in
+  "Test Alpha renaming" >:::
+  [
+    "test fresh_name, no change" >::
+    (test_fresh "name1" ["x"; "y"; "z"] "name1");
+
+    "test fresh_name" >::
+    (test_fresh "x" ["x"; "x0"; "x1"; "x2"] "x3");
+
+    "test fresh_name" >::
+    (test_fresh "%123var" ["%123var"] "%123var0");
+
+    "not all list needs to rename" >::
+    (test_rename_list
+       ["x"; "y"; "z"] (* args *)
+       ["y"; "z"; "t"] (* space *)
+       ["x"; "y0"; "z0"] (* new names *)
+    );
+
+    "rename1" >::
+    (cmp
+       ["x"; "y"; "t"]
+       "func(x) { x := t }"
+       "func(x0) { x0 := t }");
+    
+    "rename when let rebinds" >::
+    (cmp
+       ["x"; "y"; "t"]
+       "func(x) {
          let (x = 2)
             t();
          x
        }"
-      "func(x1) {
-         let (x2 = 2)
+       "func(x0) {
+         let (x1 = 2)
             t();
-         x1
+         x0
        }");
-
-   (rename
-      ["x"; "y"; "t"]
-      "func(x) {
+    
+    "rebind names in the namespace 1" >::
+    (cmp
+       ["x"; "y"; "t"]
+       "func(x) {
          let (y = 2)
            t();
          x
        }"
-      "func(x1) {
-         let (y1 = 2)
+       "func(x0) {
+         let (y0 = 2)
            t();
-         x1
+         x0
+       }");
+    
+    "rebind names in the namespace 2" >::
+    (cmp
+       ["x"; "y"; "t"]
+       "func(x) {
+         let (y = 2)
+         let (y = 3)
+         let (y = 4)
+           t();
+         x
+       }"
+       "func(x0) {
+         let (y0 = 2)
+         let (y1 = 3)
+         let (y2 = 4)
+           t();
+         x0
+       }");
+
+    "func in func" >::
+    (cmp
+      ["n1"; "n2"; "n3"]
+      "func(n1, n2) {
+        n1;
+        func(n1, n2) {
+          prim('+', n1, n3)
+        }(n1, n2)
+       }"
+      "func(n4, n5) {
+        n4;
+        func(n6, n7) {
+          prim('+', n6, n3)
+        }(n4, n5)
        }");
   ]
 
 let _ =
   run_test_tt_main suite
+
+  
